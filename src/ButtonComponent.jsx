1381a5a2902d102806545d4592eed48267302e35
donatto22 <ottanod22@gmail.com>
2023-09-14 17:38:15 -0500

    Primeros archivos para el button component

---
+++ src/ButtonComponent.jsx
@@ -0,0 +1,9 @@
+const ButtonComponent = ({ 
+    icon, iconPosition = 'left', style = 'default', boxShadow = true, disabled = false
+}) => {
+    return (
+        <>botón</>
+    )
+}
+
+export default ButtonComponent
